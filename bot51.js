ba64c7efbef1f8bdbc1f5e424a084c5a8c063e56
Mohamed <42390271+M7MEDYT2019@users.noreply.github.com>
2018-11-29 21:27:29 +0200

    Update bot51.js

--- bot51.js
+++ bot51.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+onst Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -6,56 +6,52 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "495608433064673281"; // ايدي السررفر
-var channel = "505518413213597706";//ايدي الروم
+var server = "515893561645072386"; // ايدي السررفر
+var channel = "516209355167760405";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('** فرعونفؤعونفؤعونفؤعونفؤعونفؤعونفرعونفؤعونفؤعونفؤعونفؤعونفرعونفرعونفرعونفؤعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفؤعونفؤعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفؤعونفرعونفرعونفؤعونفرعونفرعونفؤعونفؤعونفؤعونفؤعونفؤعونفؤعونفرعونفرعونفرعونفرعونفرعونفؤعونفرعونفرعونفرعومفرعون **')
+    client.guilds.get(server).channels.get(channel).send('** محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد **')
     },305);
 })
 
 
 
-client.on('ready', async() => {
-var server = "495608433064673281"; // ايدي السررفر
-var channel = "505518413213597706";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#daily')
-    },8.64e+7);
-})
-
-client.on('ready', async() => {
-var server = "495608433064673281"; // ايدي السررفر
-var channel = "505518413213597706";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#rep <@314135031029170197>')
-    },8.64e+7);
-})
-
-
 client.on('message', message => {
 if(message.content.startsWith('51s')) {
-if(message.author.id !== "314135031029170197") return;
+if(message.author.id !== "502474928466165760") return;
 var args = message.content.split(' ').slice(1).join(' ');
 message.channel.send(args);
 }
 });
  
+
 client.on('message', message => {
-if(message.content.startsWith('5s')) {
-if(message.author.id !== "314135031029170197") return;
-var args = message.content.split(' ').slice(1).join(' ');
-message.channel.send(args);
-}
+	if(message.content === '1'){
+        message.channel.send('#daily')
+    }
 });
- 
+
 client.on('message', message => {
-if(message.content.startsWith('51s')) {
-if(message.author.id !== "420637382644072451") return;
-var args = message.content.split(' ').slice(1).join(' ');
-message.channel.send(args);
-}
+	if(message.content === '2'){
+        message.channel.send('#credit')
+    }
 });
- 
+
+client.on('message', message => {
+	if(message.content === '3'){
+        message.channel.send('#rep <@420637382644072451>')
+    }
+});
+
+client.on('message', message => {
+     if(message.content === '4'){
+        message.member.voiceChannel.join();
+    }
+});
+
+
+
+
+client.login(process.env.BOT_TOKEN44)
 
 client.on('message', message => {
 	if(message.content === '1'){
